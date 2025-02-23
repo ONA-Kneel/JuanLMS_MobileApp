a8c1513ab2e817cb6bd0b4d6b0d6351b9f4f5e27
Nyyline <nicolettecborre@gmail.com>
2025-02-23 16:25:28 +0800

    Update App.js

--- JuanLMS_Mobile/App.js
+++ JuanLMS_Mobile/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>JuanLMS: Study,Perform,Execute</Text>
+      <Text>JuanLMS: Study,Perform,Execute</Text>
       <StatusBar style="auto" />
     </View>
   );
