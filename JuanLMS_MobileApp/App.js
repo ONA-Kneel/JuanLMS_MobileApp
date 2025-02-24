f2208581be22a113c3182a35069b81aaf596d322
Nyyline <nicolettecborre@gmail.com>
2025-02-24 18:24:46 +0800

    DELETE KO YUNG UMPISANG ANO KASI AYAW NYA MAG DL NG NPX

--- JuanLMS_MobileApp/App.js
+++ JuanLMS_MobileApp/App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>JuanLMS: Study,Perform,Execute</Text>
-      <Text>JuanLMS: Study,Perform,Execute</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
