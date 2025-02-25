50e2c34ca7095c562baa301f7fd5db8e416cad4a
Nyyline <nicolettecborre@gmail.com>
2025-02-25 18:12:03 +0800

    AAAAAAAAAAA NASUSUKA NA AKOOO

---
+++ JuanLMS_MobileApp/components/Students/StudentsChats.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { LoginStyleheet, Text, TouchableOpacity, View } from 'react-native';
+import { CheckBox, Image, ImageBackground, ProgressBar, ScrollView, TextInput } from 'react-native-web';
+import * as React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import StudentActsStyle from '../styles/Stud/StudentActsStyle';
+
+const Tab = createMaterialTopTabNavigator()
+
+
+export default function StudentChats() {
+  //navigation
+  const changeScreen = useNavigation();
+    
+  const back =()=>{
+        changeScreen.navigate("SDash")
+      }
+
+  return (
+    <View>
+      
+
+
+    </View>
+
+    // <NavigationContainer>
+    //   <Tabs />
+    // </NavigationContainer>
+  );
+}
